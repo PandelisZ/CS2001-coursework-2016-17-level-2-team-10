22a5035afaab6df1b208fcda71db64fe96b9b361
Subam <bista.subam@gmail.com>
2016-12-06 11:36:55 +0000

    Created Movie/Tv show page with dummy data and youtube api

---
+++ app/src/main/java/com/example/com/reelreviews/Config.java
@@ -0,0 +1,12 @@
+package com.example.com.reelreviews;
+
+/**
+ * Created by subam on 04/12/16.
+ */
+
+public final class Config {
+
+
+    public static final String API_KEY = "AIzaSyC8frZQ_xjSeSqeBJIpkPnnKkljmIyvPTw";
+    public static String video_String = "_MC3XuMvsDI";
+}
