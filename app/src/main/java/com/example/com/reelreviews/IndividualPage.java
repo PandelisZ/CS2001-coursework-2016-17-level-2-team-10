4d79355db64c69d4f6275a28f7d1e00a1aa7f339
Subam <bista.subam@gmail.com>
2016-12-11 15:55:38 +0000

    Share Button Added

--- app/src/main/java/com/example/com/reelreviews/IndividualPage.java
+++ app/src/main/java/com/example/com/reelreviews/IndividualPage.java
@@ -1,16 +1,21 @@
 package com.example.com.reelreviews;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+import org.w3c.dom.Text;
 
 public class IndividualPage extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
 
@@ -42,8 +47,33 @@ public class IndividualPage extends AppCompatActivity implements YouTubePlayer.O
         String castMembers = TextUtils.join("\n", castMemebersList);
 
         //Locates the text view to display the cast members
-        TextView castMember = (TextView) findViewById(R.id.cast);
+        final TextView castMember = (TextView) findViewById(R.id.cast);
         castMember.setText(castMembers);
+
+        /**
+         * Listener for Share button
+         * Shares the info about the movie through intents
+         * Opens a dialog for the user to choose the platfrom to share
+         */
+
+        Button shareButton = (Button) findViewById(R.id.share_Button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Text view object to get the title anme*/
+                TextView title = (TextView) findViewById(R.id.title);
+
+                /**
+                 * Creating intents to share and population menu
+                 */
+                Intent optionInflator = null;
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.putExtra(Intent.EXTRA_TEXT, title.getText().toString());
+                intent.setType("text/plain");
+                optionInflator = Intent.createChooser(intent, "Share");
+                startActivity(optionInflator);
+            }
+        });
 
     }
 
