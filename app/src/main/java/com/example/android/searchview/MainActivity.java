429da7b0505277ee6858ec5f81de4cab17d2e8e5
Bashiir <1510914@my.brunel.ac.uk>
2017-02-02 14:13:42 +0000

    initial commit

---
+++ app/src/main/java/com/example/android/searchview/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.android.searchview;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.zip.Inflater;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView b =(ListView) findViewById(R.id.search_movies);
+        ArrayList<String> arrayMovies = new ArrayList<>();
+        arrayMovies.addAll(Arrays.asList(getResources().getStringArray(R.array.array_movies)));
+
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayMovies);
+
+         b.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView) item.getActionView();
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+             adapter.getFilter().filter(newText);
+
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
+
+
+
+
+    }
+}
