cc8c98b196b18835cd433933d8efe03b55839448
subam <bista.subam@gmail.com>
2017-02-27 20:10:05 +0000

    Adding pages

---
+++ app/src/main/java/com/example/com/reelreviews/Config.java
@@ -0,0 +1,11 @@
+package com.example.com.reelreviews;
+
+/**
+ * Created by subam on 04/12/16.
+ */
+
+public class  Config {
+
+    public static final String API_KEY = "AIzaSyC8frZQ_xjSeSqeBJIpkPnnKkljmIyvPTw";
+
+}
